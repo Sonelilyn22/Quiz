a4f41cb25210cb628b40e6312ee9961c4d62c6fd
Sonelilyn22 <kalerworld23@gmail.com>
2025-08-26 20:54:50 +0400

    ChangesToClasses

--- main.py
+++ main.py
@@ -18,67 +18,20 @@ def depricated_form():
     vl1.addWidget(passfield)
     vl1.addWidget(authbutton)#Добавляем на линию кнопку
     window.show()
-def auth():
-    global window
-    login = window.input_login.text()
-    password = window.input_pass.text()
-    if login != '':
-        if password!='':
-            query = f'''login = '{login}' and password = '{password}' '''
-            result = db.get_one_by_filter('users',query)
-            if len(result) > 0:
-                global user
-                user = usermodel.user()
-                user.createUser(result)
-                set_user(user.id)
-                window = GeneralUi()
-        else:
-            print('Пароль не заполнен')
-    else:
-        print('Поле логин не заполнено')
-def reg():
-    login = window.input_login.text()
-    password = window.input_pass.text()
-    password2 = window.input_repeat_pass.text()
-    birthdate = to_sql_date(window.input_birthday.text())
-    email = window.input_email.text()
-    if login != '':
-        if password!='':
-            if password2 != '' and password2 == password:
-                query = f'''(login,password,birthdate,email)
-                VALUES('{login}','{password}','{birthdate}','{email}')'''
-                db.insert_one('users',query)
-                login_page()
-        else:
-            print('Пароль не заполнен')
-    else:
-        print('Поле логин не заполнено')
 def to_sql_date(date):
     year = date[len(date)-4:]
     month = date[3:5]
     day = date[0:2]
     sqldate = f'{year}-{month}-{day}'
     print(sqldate)
-    return sqldate
-def login_page():
-    global window
-    window = uic.loadUi('auth_form.ui')
-    window.auth_button.clicked.connect(auth)#На кнопку входа добавили событие при нажатии
-    window.to_reg_button.clicked.connect(to_reg_tab)
-    window.show()
-def to_reg_tab():
-   global window
-   window = uic.loadUi('reg_form.ui')
-   window.reg_button.clicked.connect(reg)
-   window.show()
+    return sqldate   
 def get_user():
-    global user #Переменная которая будет хранить все поля пользователя
     file = open('user.json','r')#Открывает файл в котором хранится id пользователя
     json_user = json.load(file)#Создаём словарь из файла
     try:
         id = json_user['id']#Получаем id по ключу
         sql_user = db.get_one_by_id('users',id)#Делаем запрос к бд
-        if len(sql_user) > 0:#Если данные вернулись создадим пользователя
+        if sql_user != None:#Если данные вернулись создадим пользователя
             user = usermodel.user()
             user.createUser(sql_user)
         else:
@@ -86,6 +39,7 @@ def get_user():
     except:#При появлении любой ошибки выполнения в try
         print('При получении id произошла ошибка')
         user = None #Зададим пользователя как None
+    return user
 def set_user(id = None):
     file = open('user.json','w')#Открываем файл
     if id != None:#Если id задан указываем этот id как значение
@@ -93,29 +47,76 @@ def set_user(id = None):
     else:#Если нет то созадём пустой словарь
         user = {}
     json.dump(user,file)#Записываем наш словарь  
-def start_up():
-    get_user()
-    global user
-    if user == None:
-        login_page()
-    else:
-        global window
-        window = GeneralUi()
-
-
-class GeneralUi(QtWidgets.QWidget):
+class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
-        self.ui = uic.loadUi('general.ui')
-        self.username = self.ui.label_username
-        global user
-        self.username.setText(user.login)
-        self.ui.exit_button.clicked.connect(self.exit)#Обработка нажатия на кнопку выход
-        self.ui.show()
+        self.start_up()
+        self.show()
+        self.setCentralWidget(self.ui)
+    def start_up(self):
+        self.user = get_user()
+        if self.user == None:
+            self.ui = LoginUi()
+        else:
+            self.ui = GeneralUi(self.user)
+class GeneralUi(QtWidgets.QWidget):
+    def __init__(self,user:usermodel.user):
+        super().__init__()
+        uic.loadUi('general.ui',self)
+        self.findChild(QtWidgets.QLabel,'label_username').setText(user.login)#Задаём текст для надписи
+        self.findChild(QtWidgets.QPushButton,'exit_button').clicked.connect(self.exit)#Задаём выход для кнопки
     def exit(self):
         set_user()
-        login_page()
-
-
-start_up()
+        mainwindow.ui = LoginUi()
+        mainwindow.setCentralWidget(mainwindow.ui)
+class LoginUi(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('auth_form.ui',self)
+        self.findChild(QtWidgets.QPushButton,'auth_button').clicked.connect(self.auth)#На кнопку входа добавили событие при нажатии
+        self.findChild(QtWidgets.QPushButton,'to_reg_button').clicked.connect(self.to_reg)
+    def auth(self):
+        login = self.findChild(QtWidgets.QLineEdit,'input_login').text()
+        password = self.findChild(QtWidgets.QLineEdit,'input_pass').text()
+        if login != '':
+            if password!='':
+                query = f'''login = '{login}' and password = '{password}' '''
+                result = db.get_one_by_filter('users',query)
+                if result != None:
+                    mainwindow.user = usermodel.user()
+                    mainwindow.user.createUser(result)
+                    set_user(mainwindow.user.id)
+                    mainwindow.ui = GeneralUi(mainwindow.user)
+                    mainwindow.setCentralWidget(mainwindow.ui)
+            else:
+                print('Пароль не заполнен')
+        else:
+            print('Поле логин не заполнено')
+    def to_reg(self):
+        mainwindow.ui = RegUi()
+        mainwindow.setCentralWidget(mainwindow.ui)
+class RegUi(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('reg_form.ui',self)
+        self.findChild(QtWidgets.QPushButton,'reg_button').clicked.connect(self.reg)
+    def reg(self):
+        login = self.findChild(QtWidgets.QLineEdit,'input_login').text()
+        password = self.findChild(QtWidgets.QLineEdit,'input_pass').text()
+        password2 = self.findChild(QtWidgets.QLineEdit,'input_repeat_pass').text()
+        birthdate = to_sql_date(self.findChild(QtWidgets.QDateEdit,'input_birthday').text())
+        email = self.findChild(QtWidgets.QLineEdit,'input_email').text()
+        if login != '':
+            if password!='':
+                if password2 != '' and password2 == password:
+                    query = f'''(login,password,birthdate,email)
+                    VALUES('{login}','{password}','{birthdate}','{email}')'''
+                    db.insert_one('users',query)
+                    mainwindow.ui = LoginUi()
+                    mainwindow.setCentralWidget(mainwindow.ui)
+            else:
+                print('Пароль не заполнен')
+        else:
+            print('Поле логин не заполнено')
+mainwindow = MainWindow()
 app.exec()
